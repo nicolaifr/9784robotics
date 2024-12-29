4b2e028358ccdeea0ef7618a0676c3f0f2e35f98
Nicolai <nicolailiu329@gmail.com>
2024-12-28 19:27:45 -0800

    added claw back experimented with delays

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/commands/intothedeep/auto/autoCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/commands/intothedeep/auto/autoCommands.java
@@ -1,14 +1,29 @@
 package org.firstinspires.ftc.teamcode.hardware.commands.intothedeep.auto;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.hardware.HardwareBase;
+
+import java.util.concurrent.TimeUnit;
 
 public class autoCommands extends HardwareBase {
     //all arm and claw commands for auto go here
     //NICOLAI DO THIS
     //do FSM for now
+
+
+    // switch (state)
+        //case 0:
+        //if (!inProgress)
+        //timer.reset
+        //state++
+        //case 1:
+        //if timer.time.milliseconds >= however long
+            //timer.reset()
+            //state++
+    boolean inProgress;
     clawAutoCommands claw;
     slidesAutoCommands slides;
     @Override
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/IntoTheDeepTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/IntoTheDeepTeleop.java
@@ -12,27 +12,28 @@ import org.firstinspires.ftc.teamcode.hardware.drive.DriveTrainBase;
 @TeleOp
 public class IntoTheDeepTeleop extends OpMode {
     DriveTrainBase drive;
+    ClawBase claw;
 
     int clawState;
     @Override
     public void init() {
         drive = new DriveTrainBase();
+        claw = new ClawBase();
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         drive.init(hardwareMap, telemetry);
+        claw.init(hardwareMap, telemetry);
     }
 
     @Override
     public void loop() {
         drive.driveJoystick(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-        armControls();
         clawControls();
         getTelemetry();
     }
-
-    public void armControls() {
-    }
     public void clawControls() {
+        claw.clawClamp(gamepad2.right_stick_button);
+        claw.clawWrist(gamepad2.right_stick_x);
     }
 
     public void getTelemetry() {
