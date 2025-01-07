9b331d42eac210644eef74c6154c155ad0d806e4
forestfu <40225127+forestfu@users.noreply.github.com>
2025-01-06 17:37:32 -0800

    horiz slides and vert slides fixed and work fixed testServo.java added PIDF for pivot

---
+++ TeamCode/src/main/java/hardware/claw/OutTake.java
@@ -0,0 +1,62 @@
+package opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import hardware.HardwareBase;
+
+@TeleOp
+public class OutTake extends HardwareBase {
+
+    Servo testServo;
+    Servo testServo1;
+    Servo wrist;
+    Servo clamp;
+
+    public double armPos;
+    public double wristPos;
+
+    public int clawState;
+    boolean clampClosed;
+    @Override
+    public void init(HardwareMap ahwMap, Telemetry t) {
+        super.init(HardwareMap, Telemetry);
+        testServo = ahwMap.get(Servo.class, "ArmLeft");
+        testServo1 = ahwMap.get(Servo.class, "ArmRight");
+        wrist = ahwMap.get(Servo.class, "clawClamp");
+        clamp = ahwMap.get(Servo.class, "clawXRotate");
+
+        armPos = 0;
+        wristPos = 0;
+        clawState = 0;
+        clampClosed = false;
+    }
+    public void clamp(boolean rb, boolean lb){
+        if(rb) {
+            clamp.setPosition(1);
+        } else if (lb) {
+            clamp.setPosition(0);
+        }
+    }
+    public void arm(boolean a, boolean b) {
+        if (b && armPos != 1) {
+            testServo.setPosition(armPos+= 0.001);
+            testServo1.setPosition(armPos += 0.001);
+        }
+        if (a && armPos != 0) {
+            testServo.setPosition(armPos -= 0.001);
+            testServo1.setPosition(armPos -= 0.001);
+        }
+    }
+
+    public void wrist(double rt, double lt) {
+        if(rt >= 0.5 && wristPos != 1) {
+            wrist.setPosition(wristPos += 0.003);
+        } else if (lt >= 0.5 && wristPos != 0) {
+            wrist.setPosition(wristPos -= 0.003);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/hardware/claw/PIDF_Pivot.java
@@ -0,0 +1,68 @@
+package hardware.claw;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class PIDF_Pivot extends OpMode {
+    private PIDController controller;
+    //P,I,D in the PID controller watch KookyBotz Video for more info
+    public static double p = 0, i = 0, d = 0;
+    //feedforward
+    public static double f = 0;
+    //arm target position
+    public static int target = 0;
+    //how many ticks in degree USING REV THROUGH BORE ENCODER
+    private final double ticks_in_degree = ((double) 8192) /360;
+    private DcMotorEx pivot;
+
+    @Override
+    public void init() {
+        //initialization code when "INIT" is pressed
+        controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        pivot = hardwareMap.get(DcMotorEx.class, "pivotMotor");
+        pivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    }
+
+    @Override
+    public void loop() {
+        //loop code when "PLAY/Triangle" is hit loops over again while opmode is active
+        controller.setPID(p, i, d);
+        int armPos = pivot.getCurrentPosition();
+        //PID MATH
+        double pid = controller.calculate(armPos, target);
+        //feedforward math
+        double ff = Math.cos(Math.toRadians(target/ticks_in_degree)) * f;
+        //power calculated
+        double power = pid + ff;
+        //setting motor power after all those calculations
+        pivot.setPower(power);
+        //telemetry for tuning
+        telemetry.addData("pos", armPos);
+        telemetry.addData("target", target);
+        telemetry.update();
+    }
+
+    public int getTarget() {
+        telemetry.addData("target", target);
+        telemetry.update();
+        return target;
+    }
+
+    public void setTarget(int newTarget){
+        target = newTarget;
+    }
+
+    public void rotateToPos (int rotateTarget) {
+        target = rotateTarget;
+        loop();
+    }
+}
+
+}
