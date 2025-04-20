cf83c1d9a45537a9421776b5da8eca4727e31956
forestfu <40225127+forestfu@users.noreply.github.com>
2025-04-19 22:45:32 -0700

    pedro path gone

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/armclaw/ClawBase.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.hardware.armclaw;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.hardware.HardwareBase;
+
+public class ClawBase extends HardwareBase {
+    public Servo clawClamp;
+    public Servo clawWrist;
+
+    public double currentPos;
+
+    @Override
+    public void init(HardwareMap ahwMap, Telemetry t) {
+        super.init(ahwMap, t);
+        clawClamp = ahwMap.get(Servo.class, "clawClamp");
+        clawWrist = ahwMap.get(Servo.class, "clawWrist");
+    }
+
+    public void clawClamp(double rightJoystickY, double currentPos) {
+        if (rightJoystickY >= 0.25 && currentPos < 1) {
+            currentPos += 0.1;
+            clawClamp.setPosition(currentPos);
+        } else if (rightJoystickY <= -0.25 && currentPos > 0) {
+            currentPos -= 0.1;
+            clawClamp.setPosition(currentPos);
+        }
+    }
+
+    public void clawWrist(double leftJoystickX, double currentPos) {
+        if (leftJoystickX >= 0.25 && currentPos < 1) {
+            currentPos += 0.1;
+            currentPos = this.currentPos;
+            clawWrist.setPosition(currentPos);
+        } else if (leftJoystickX <= -0.25 && currentPos > 0){
+            currentPos -= 0.1;
+            currentPos = this.currentPos;
+            clawWrist.setPosition(currentPos);
+        }
+    }
+
+    public void closeClamp() {
+        clawClamp.setPosition(1);
+    }
+    public void openClamp() {
+        clawClamp.setPosition(0);
+    }
+}
